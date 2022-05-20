243556d827b343fbfe5dcb2bf7780a62cdf5e8ed
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-05-20 22:08:32 +0530

    Added Boilerplate

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 import items from './items.js';
 
 
-function initialise(){
-    let listContainer=document.querySelector('.list');
+function getShortenedTitle(title) {
+    return title;
+}
+
+
+function initialise() {
+    let listContainer = document.querySelector('.list');
     listContainer.childNodes[1].classList.add('active');
-    let image=document.querySelector('.image');
-    image.setAttribute('src',items[currentIndex-1].previewImage);
-    let text=document.querySelector('.text');
-    text.innerHTML=items[currentIndex-1].title;
+    let image = document.querySelector('.image');
+    image.setAttribute('src', items[currentIndex - 1].previewImage);
+    let text = document.querySelector('.text');
+    text.innerHTML = getShortenedTitle(items[currentIndex - 1].title);
 }
 
 
 
 
-function updateSelection(currentIndex,newIndex,length){
+function updateSelection(currentIndex, newIndex, length) {
     //Index Cannot go outside of the bounds
-    if(newIndex>length || newIndex<1){    
+    if (newIndex > length || newIndex < 1) {
         return;
     }
-    let listContainer=document.querySelector('.list');
+    let listContainer = document.querySelector('.list');
     listContainer.childNodes[currentIndex].classList.toggle('active');
     listContainer.childNodes[newIndex].classList.toggle('active');
-    let image=document.querySelector('.image');
-    image.setAttribute('src',items[newIndex-1].previewImage);
-    let text=document.querySelector('.text');
-    text.innerHTML=items[newIndex-1].title;
+    let image = document.querySelector('.image');
+    image.setAttribute('src', items[newIndex - 1].previewImage);
+    let text = document.querySelector('.text');
+    text.innerHTML = getShortenedTitle(items[newIndex - 1].title);
 }
 
-var length=items.length;
-let currentIndex=1;
-let listContainer=document.querySelector('.list');
-let fragment=document.createDocumentFragment();
+var length = items.length;
+let currentIndex = 1;
+let listContainer = document.querySelector('.list');
+let fragment = document.createDocumentFragment();
 
-items.forEach((item)=>{
+items.forEach((item) => {
     //Creating a wrapper
-    let outerDiv=document.createElement('div');
+    let outerDiv = document.createElement('div');
     //Adding the class outerdiv to the outermost div containing the icon and the title
     outerDiv.classList.add('outerdiv');
     //Creating the icon of the image
-    let icon=document.createElement('img');
+    let icon = document.createElement('img');
     icon.classList.add('icon');
-    icon.setAttribute('src',item.previewImage);
+    icon.setAttribute('src', item.previewImage);
     // Creating a div for title
-    let title=document.createElement('div');
+    let title = document.createElement('div');
     title.classList.add('title');
-    title.innerHTML=item.title;
+    title.innerHTML = getShortenedTitle(item.title);
     //Appending icon to outerdiv
     outerDiv.appendChild(icon);
     //Appending title to outerdiv
@@ -64,8 +69,8 @@ document.querySelectorAll('.outerdiv').forEach(item => {
     item.addEventListener('click', event => {
         let parent = item.parentNode;
         var index = Array.prototype.indexOf.call(parent.children, item);
-        updateSelection(currentIndex,Number(index)+1,length);
-        currentIndex=Number(index)+1;
+        updateSelection(currentIndex, Number(index) + 1, length);
+        currentIndex = Number(index) + 1;
     })
 })
 
@@ -73,19 +78,18 @@ document.querySelectorAll('.outerdiv').forEach(item => {
 
 
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     switch (event.keyCode) {
         case 38:
-            if(currentIndex>1){
-                updateSelection(currentIndex,currentIndex-1,length);
-                currentIndex=currentIndex-1;
+            if (currentIndex > 1) {
+                updateSelection(currentIndex, currentIndex - 1, length);
+                currentIndex = currentIndex - 1;
             }
             break;
         case 40:
-            if(currentIndex<length)
-            {
-                updateSelection(currentIndex,currentIndex+1,length);
-                currentIndex=currentIndex+1;
+            if (currentIndex < length) {
+                updateSelection(currentIndex, currentIndex + 1, length);
+                currentIndex = currentIndex + 1;
             }
             break;
     }
